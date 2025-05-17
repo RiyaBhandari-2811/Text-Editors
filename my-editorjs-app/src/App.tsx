aa91c530e7c044265698d35230de2a87de6dfdbd
Riya <bhandari28riya@gmail.com>
2025-05-17 11:44:09 +0530

    added the reactjs imple

--- my-editorjs-app/src/App.tsx
+++ my-editorjs-app/src/App.tsx
@@ -1,7 +1,22 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+// App.tsx
+import React, { useState } from "react";
+import Editor from "./component/Editor";
+import { type OutputData } from "@editorjs/editorjs";
 
-export default App
+const App: React.FC = () => {
+  const [editorData, setEditorData] = useState<OutputData | null>(null);
+
+  const handleEditorChange = (data: OutputData) => {
+    setEditorData(data);
+    console.log("Editor Data:", data);
+  };
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Editor.js in React + TypeScript</h1>
+      <Editor data={editorData ?? undefined} onChange={handleEditorChange} />
+    </div>
+  );
+};
+
+export default App;
