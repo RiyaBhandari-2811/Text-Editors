97ff825a6b8ba53a092af5f05f1e756afba8e423
Riya <bhandari28riya@gmail.com>
2025-05-17 09:03:30 +0530

    not going with react lib

--- my-editorjs-app/src/component/Editor.tsx
+++ my-editorjs-app/src/component/Editor.tsx
@@ -1,18 +1,19 @@
-import EditorJS from "@editorjs/editorjs";
-import EJHeader from "@editorjs/header";
-import EJList from "@editorjs/list";
+import { createReactEditorJS } from "react-editor-js";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
 
-const Editor = () => {
-  const editor = new EditorJS({
-    holder: "editor-js",
-    tools: {
-      EJHeader,
-      EJList,
-    },
-    autofocus: true,
-    placeholder: "Let's write an awsome Article",
-  });
-  return <div>Editor</div>;
+const ReactEditorJS = createReactEditorJS();
+
+const tools = {
+  header: Header,
+  list: List,
 };
 
-export default Editor;
+const MyEditor = () => {
+  const handleInitialize = (instance) => {
+    console.log("Editor.js instance:", instance);
+  };
+
+  return <ReactEditorJS tools={tools} onInitialize={handleInitialize} />;
+};
+export default MyEditor;
